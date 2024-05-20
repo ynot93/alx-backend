b5955b844998e1f9c4dee6c66576e721a95ec193
Tony Mputhia <tonymmputhia@gmail.com>
2024-05-20 23:38:38 +0300

    Calculate index range

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+This module deals with pagination of datasets
+
+"""
+
+def index_range(page, page_size):
+    """
+    Returns a tuple containing range of pagination params.
+
+    """
+    offset = (page - 1) * page_size
+    return (offset, offset + page_size)
