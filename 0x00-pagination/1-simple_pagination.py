5ac8c414be5b4c84725c3f79ed811df652eb6f47
Tony Mputhia <tonymmputhia@gmail.com>
2024-05-21 12:54:07 +0300

    Add documentation for get_page function

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -37,6 +37,10 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Returns the dataset in thae range requested
+
+        """
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
 
