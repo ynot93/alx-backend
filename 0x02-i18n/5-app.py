cb365d6aca15c1e88999f606a6d2624764ea7ec9
Tony Mputhia <tonymmputhia@gmail.com>
2024-05-28 23:12:10 +0300

    Mock logging in

---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+"""
+This module provides support for internationalization(i18n)
+
+"""
+from flask import Flask, render_template, request, g
+from flask_babel import Babel, gettext as _
+
+app = Flask(__name__)
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+class Config():
+    """
+    Configure Babel's default locale and timezone
+
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """
+    Determine language from the accepted ones
+
+    """
+    # Check if a user is logged in and has a locale set
+    user = getattr(g, 'user', None)
+    if user and user['locale'] in app.config['LANGUAGES']:
+        return user['locale']
+
+    # Check if 'locale' is in the request rags
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+# Define the get_user function
+def get_user():
+    user_id = request.args.get('login_as')
+    if user_id:
+        return users.get(int(user_id))
+    return None
+
+# Define the before_request function
+@app.before_request
+def before_request():
+    g.user = get_user()
+
+
+@app.route("/")
+def index():
+    """
+    Entry point into the flask app
+
+    """
+    return render_template('3-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
