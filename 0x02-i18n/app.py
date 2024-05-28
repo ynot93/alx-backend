346db0304b90ed2e44b307e0e1b8f84a5c14675e
Tony Mputhia <tonymmputhia@gmail.com>
2024-05-28 23:34:46 +0300

    Display the current time

---
+++ 0x02-i18n/app.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+"""
+This module provides support for internationalization(i18n)
+
+"""
+from flask import Flask, render_template, request, g
+from flask_babel import Babel, gettext as _
+import pytz
+from datetime import datetime
+
+app = Flask(__name__)
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+class Config():
+    """
+    Configure Babel's default locale and timezone
+
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """
+    Determine language from the accepted ones
+
+    """
+    # Check if 'locale' is in the request rags
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
+
+    # Check if a user is logged in and has a locale set
+    user = getattr(g, 'user', None)
+    if user and user['locale'] in app.config['LANGUAGES']:
+        return user['locale']
+
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@babel.timezoneselector
+def get_timezone():
+    timezone = request.args.get('timezone')
+    if timezone:
+        try:
+            return pytz.timezone(timezone).zone
+        except pytz.exceptions.UnknownTimeZoneError:
+            pass
+
+    user = getattr(g, 'user', None)
+    if user and user['timezone']:
+        try:
+            return pytz.timezone(user['timezone']).zone
+        except pytz.exceptions.UnknownTimeZoneError:
+            pass
+
+    return app.config['BABEL_DEFAULT_TIMEZONE']
+
+
+# Define the get_user function
+def get_user():
+    user_id = request.args.get('login_as')
+    if user_id:
+        return users.get(int(user_id))
+    return None
+
+# Define the before_request function
+@app.before_request
+def before_request():
+    g.user = get_user()
+
+
+@app.route("/")
+def index():
+    """
+    Entry point into the flask app
+
+    """
+    current_time = datetime.now(pytz.timezone(get_timezone())).strftime('%b %d, %Y, %I:%M:%S %p')
+
+    return render_template('index.html', current_time=current_time)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
