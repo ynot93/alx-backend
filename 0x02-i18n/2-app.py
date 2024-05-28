73ca7bf77670267ca8335274d8e05cf9cdd72f2c
Tony Mputhia <tonymmputhia@gmail.com>
2024-05-28 22:17:36 +0300

    Get locale from requests

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -3,7 +3,7 @@
 This module provides support for internationalization(i18n)
 
 """
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_babel import Babel
 
 app = Flask(__name__)
@@ -23,13 +23,21 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
+@babel.localeselector
+def get_locale():
+    """
+    Determine language from the accepted ones
+
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
 @app.route("/")
 def index():
     """
     Entry point into the flask app
 
     """
-    return render_template('1-index.html')
+    return render_template('2-index.html')
 
 
 if __name__ == "__main__":
