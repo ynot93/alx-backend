a0ae66c4f06274370db088cd7f76dc0fc044e857
Tony Mputhia <tonymmputhia@gmail.com>
2024-05-28 22:45:03 +0300

    Use gettext to parametrize the templates

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""
+This module provides support for internationalization(i18n)
+
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel, gettext as _
+
+app = Flask(__name__)
+
+
+class Config():
+    """
+    Configure Babel's default locale and timezone
+
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """
+    Determine language from the accepted ones
+
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route("/")
+def index():
+    """
+    Entry point into the flask app
+
+    """
+    return render_template('3-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
